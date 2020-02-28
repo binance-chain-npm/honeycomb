af6fa817e32171145a84cfd66c36a244b4a0ad33
Rubén Illodo Brea <me@rubenillodo.com>
2020-02-28 16:04:15 +0100

    fix(Tooltip): Better default styles

--- src/components/Tooltip/styles.tsx
+++ src/components/Tooltip/styles.tsx
@@ -5,5 +5,8 @@ import { baseStyles } from '../../modules/core';
 export const Styles = css`
   .tippy-tooltip {
     ${baseStyles};
+    background: ${({ theme }) => theme.honeycomb.color.secondary};
+    color: ${({ theme }) => theme.honeycomb.color.readable(theme.honeycomb.color.secondary)};
+    border-radius: 0;
   }
 `;
