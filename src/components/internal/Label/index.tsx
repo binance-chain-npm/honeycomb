cb53ac655f88beae65d03ddec3f0501f271c56c1
Rubén Illodo Brea <me@rubenillodo.com>
2020-02-28 15:33:55 +0100

    fix(Label): Applied base styles

--- src/components/internal/Label/index.tsx
+++ src/components/internal/Label/index.tsx
@@ -1,7 +1,11 @@
 import styled from 'styled-components';
 import { em } from 'polished';
 
+import { baseStyles } from '../../../modules/core';
+
 export const Label = styled.label`
+  ${baseStyles};
+
   display: block;
   font-size: ${({ theme }) => em(theme.honeycomb.fontSize.small)};
   font-weight: 600;
