0dda3e5cdbc587b9ed27cd224a2aa560b5e3a9f8
Rubén Illodo Brea <me@rubenillodo.com>
2020-03-02 16:45:58 +0100

    feat(PasswordInput): Rules can be disabled

--- src/components/PasswordInput/component.tsx
+++ src/components/PasswordInput/component.tsx
@@ -3,19 +3,35 @@ import React, { useState, useMemo, useEffect, useCallback } from 'react';
 import { Testable } from '../../modules/test-ids';
 import { TextInput } from '../TextInput';
 import { Tooltip } from '../Tooltip';
+import { Icon } from '../Icon';
+import { Styleless } from '../Styleless';
 
 import { List, Item } from './styled';
 
-const MIN_LENGTH = 8;
+export type Props = Omit<React.ComponentProps<typeof TextInput>, 'type'> &
+  Testable & {
+    minLenght: number;
+    mustHaveUpperCase: boolean;
+    mustHaveSymbol: boolean;
+    mustHaveDigit: boolean;
+  };
 
-export type Props = Omit<React.ComponentProps<typeof TextInput>, 'type'> & Testable;
-
-export const Component = ({ value, onFocus, onBlur, ...otherProps }: Props) => {
+export const Component = ({
+  value,
+  onFocus,
+  onBlur,
+  minLenght,
+  mustHaveDigit,
+  mustHaveSymbol,
+  mustHaveUpperCase,
+  ...otherProps
+}: Props) => {
   const [isLongEnough, setIsLongEnough] = useState(false);
   const [hasUpperCase, setHasUpperCase] = useState(false);
   const [hasSymbol, setHasSymbol] = useState(false);
   const [hasDigit, setHasDigit] = useState(false);
   const [isFocused, setIsFocused] = useState(false);
+  const [shouldDisplay, setShouldDisplay] = useState(false);
 
   const focus = useCallback<NonNullable<Props['onFocus']>>(
     (evt) => {
@@ -42,17 +58,25 @@ export const Component = ({ value, onFocus, onBlur, ...otherProps }: Props) => {
       return;
     }
 
-    setIsLongEnough(value.length >= MIN_LENGTH);
+    setIsLongEnough(value.length >= minLenght);
     setHasUpperCase(/[A-Z]/.test(value));
     setHasSymbol(/[^a-z0-9]/i.test(value));
     setHasDigit(/[0-9]/.test(value));
-  }, [value]);
+  }, [value, minLenght]);
 
-  const isValid = useMemo(() => isLongEnough && hasUpperCase && hasSymbol && hasDigit, [
+  const isValid = useMemo(() => {
+    if (mustHaveDigit && !hasDigit) return false;
+    if (mustHaveSymbol && !hasSymbol) return false;
+    if (mustHaveUpperCase && !hasUpperCase) return false;
+    return isLongEnough;
+  }, [
     isLongEnough,
     hasUpperCase,
     hasSymbol,
     hasDigit,
+    mustHaveDigit,
+    mustHaveSymbol,
+    mustHaveUpperCase,
   ]);
 
   return (
@@ -62,9 +86,9 @@ export const Component = ({ value, onFocus, onBlur, ...otherProps }: Props) => {
           Your password must have:
           <List>
             {!isLongEnough && <Item>8 or more characters.</Item>}
-            {!hasUpperCase && <Item>At least one upper case character.</Item>}
-            {!hasDigit && <Item>At least one digit.</Item>}
-            {!hasSymbol && <Item>At least one symbol.</Item>}
+            {mustHaveUpperCase && !hasUpperCase && <Item>At least one upper case character.</Item>}
+            {mustHaveDigit && !hasDigit && <Item>At least one digit.</Item>}
+            {mustHaveSymbol && !hasSymbol && <Item>At least one symbol.</Item>}
           </List>
         </>
       }
@@ -78,14 +102,29 @@ export const Component = ({ value, onFocus, onBlur, ...otherProps }: Props) => {
         onFocus={focus}
         onBlur={blur}
         value={value}
-        type="password"
+        type={shouldDisplay ? 'text' : 'password'}
         state={isValid ? undefined : TextInput.State.Danger}
+        right={
+          <Styleless
+            tag="button"
+            onClick={() => setShouldDisplay(!shouldDisplay)}
+            style={{ fontSize: 24 }}
+          >
+            <Icon src={shouldDisplay ? Icon.Src.EyeBlocked : Icon.Src.Eye} />
+          </Styleless>
+        }
       />
     </Tooltip>
   );
 };
 
 Component.displayName = 'PasswordInput';
+Component.defaultProps = {
+  minLenght: 8,
+  mustHaveUpperCase: true,
+  mustHaveSymbol: true,
+  mustHaveDigit: true,
+};
 
 Component.Label = TextInput.Label;
 Component.Input = TextInput.Input;
