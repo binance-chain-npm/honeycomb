ae64f7661657a62c276bc4a61ab0ae1bc3dc460b
Rubén Illodo Brea <me@rubenillodo.com>
2020-03-02 11:32:24 +0100

    feat(Tooltip): `hideOnClick` prop

--- src/components/Tooltip/component.tsx
+++ src/components/Tooltip/component.tsx
@@ -13,7 +13,7 @@ export type TriggerValue = 'mouseenter' | 'click' | 'manual';
 export type Props = Pick<React.HTMLProps<HTMLElement>, 'children'> &
   Pick<
     React.ComponentProps<typeof Tippy>,
-    'arrow' | 'className' | 'theme' | 'onShow' | 'onHide' | 'visible' | 'enabled'
+    'arrow' | 'className' | 'theme' | 'onShow' | 'onHide' | 'visible' | 'enabled' | 'hideOnClick'
   > &
   Testable & {
     content: React.ReactNode;
