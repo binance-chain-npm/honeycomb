ff029c91c1c2a2daba6a7a889a039c2541e6aebe
Rubén Illodo Brea <me@rubenillodo.com>
2020-03-03 15:29:37 +0100

    fix(Tooltip): Rounded corners

--- src/components/Tooltip/styles.tsx
+++ src/components/Tooltip/styles.tsx
@@ -4,7 +4,7 @@ export const Styles = css`
   .tippy-tooltip {
     background: ${({ theme }) => theme.honeycomb.tooltip.bg};
     color: ${({ theme }) => theme.honeycomb.color.readable(theme.honeycomb.color.secondary)};
-    border-radius: 0;
+    border-radius: ${({ theme }) => theme.honeycomb.radius.normal};
     box-shadow: rgba(0, 0, 0, 0.2) 0px 2px 1px -1px, rgba(0, 0, 0, 0.14) 0px 1px 1px 0px,
       rgba(0, 0, 0, 0.12) 0px 1px 3px 0px;
   }
