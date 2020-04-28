ed396f338a61487bd54da4d66e9832d2b82a0b8c
Rubén Illodo Brea <me@rubenillodo.com>
2020-04-28 16:13:31 +0200

    feat: Added `LeftArrow` icon

--- src/components/Icon/source.tsx
+++ src/components/Icon/source.tsx
@@ -2,12 +2,14 @@ import Tick from './assets/Tick.svg';
 import CaretDown from './assets/CaretDown.svg';
 import Eye from './assets/Eye.svg';
 import EyeBlocked from './assets/EyeBlocked.svg';
+import LeftArrow from './assets/LeftArrow.svg';
 
 export enum Src {
   CaretDown = 'CaretDown',
   Tick = 'Tick',
   Eye = 'Eye',
   EyeBlocked = 'EyeBlocked',
+  LeftArrow = 'LeftArrow',
 }
 
 export const sourceFor = (src: Src) =>
@@ -16,4 +18,5 @@ export const sourceFor = (src: Src) =>
     [Src.CaretDown]: CaretDown,
     [Src.Eye]: Eye,
     [Src.EyeBlocked]: EyeBlocked,
+    [Src.LeftArrow]: LeftArrow,
   }[src]);
