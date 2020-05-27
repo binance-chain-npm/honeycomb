013dcf6de6669c362fc3a6fe42c2c722fe79abf4
Rubén Illodo Brea <me@rubenillodo.com>
2020-05-27 09:36:23 +0200

    fix: Initial render for `CopyToClipboard` icons

--- cypress/integration/CopyToClipboard.ts
+++ cypress/integration/CopyToClipboard.ts
@@ -1,31 +1,27 @@
 describe('CopyToClipboard', () => {
   it('renders properly in light theme', () => {
     cy.visitStory({ storyId: 'elements-copytoclipboard--default', themeName: 'GoldLight' });
-
-    cy.get('[data-testid="copy-to-clipboard.text"]');
-
     cy.clock();
-    cy.tick(100000);
+
+    cy.get('[data-testid="copy-to-clipboard.copy-icon"]').should('be.visible');
     cy.percySnapshot('CopyToClipboard in Gold Light theme');
 
     cy.get('[data-testid="copy-to-clipboard.text"]').click();
-
-    cy.tick(1000);
+    cy.tick(10000);
+    cy.get('[data-testid="copy-to-clipboard.tick-icon"]').should('be.visible');
     cy.percySnapshot('CopyToClipboard after clicking in Gold Light theme');
   });
 
   it('renders properly in dark theme', () => {
     cy.visitStory({ storyId: 'elements-copytoclipboard--default', themeName: 'GoldDark' });
-
-    cy.get('[data-testid="copy-to-clipboard.text"]');
-
     cy.clock();
-    cy.tick(100000);
+
+    cy.get('[data-testid="copy-to-clipboard.copy-icon"]').should('be.visible');
     cy.percySnapshot('CopyToClipboard in Gold Dark theme');
 
     cy.get('[data-testid="copy-to-clipboard.text"]').click();
-
-    cy.tick(1000);
+    cy.tick(10000);
+    cy.get('[data-testid="copy-to-clipboard.tick-icon"]').should('be.visible');
     cy.percySnapshot('CopyToClipboard after clicking in Gold Dark theme');
   });
 });
