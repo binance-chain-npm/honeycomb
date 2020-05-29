b2ef8b00d4579c5f603de76338e8696286f3da76
Rubén Illodo Brea <me@rubenillodo.com>
2020-05-29 16:04:44 +0200

    feat: Dropdown divider (#22)

---
+++ src/components/Dropdown/Divider/index.tsx
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export const Divider = styled.div`
+  background: ${({ theme }) => theme.honeycomb.color.border};
+  height: 1px;
+  width: 100%;
+`;
--- src/components/Dropdown/index.tsx
+++ src/components/Dropdown/index.tsx
@@ -1,11 +1,14 @@
 import { Component } from './component';
 import { DefaultTarget } from './DefaultTarget';
 import { Item } from './Item';
+import { Divider } from './Divider';
 
 export const Dropdown = Component as typeof Component & {
   DefaultTarget: typeof DefaultTarget;
   Item: typeof Item;
+  Divider: typeof Divider;
 };
 
 Dropdown.DefaultTarget = DefaultTarget;
 Dropdown.Item = Item;
+Dropdown.Divider = Divider;
