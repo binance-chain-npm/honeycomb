c84b886357165c674053244129b0fd4a1ba91004
Rubén Illodo Brea <me@rubenillodo.com>
2020-05-29 14:13:08 +0200

    feat: `AccountAvatar` component generated based on a string value (#21)
    * feat: `AbstractShape`
    
    * feat: With squares
    
    * refactor: Simplified code
    
    * fix: Use predefined colors
    
    * refactor: Renamed to `AccountAvatar`
    
    * test: Percy snapshot

---
+++ src/components/AccountAvatar/component.tsx
@@ -0,0 +1,34 @@
+import React, { useMemo } from 'react';
+import { SHA1 } from 'crypto-js';
+import { useTheme } from 'styled-components';
+
+import { Testable, useBuildTestId } from '../../modules/test-ids';
+
+import { Svg, TextContainer, Container } from './styled';
+import { Square } from './Square';
+
+export type Props = Testable & {
+  value: string;
+  initial?: string;
+};
+
+export const Component = ({ 'data-testid': testId, value, initial: initialParam }: Props) => {
+  const theme = useTheme();
+  const buildTestId = useBuildTestId(testId);
+  const initial = initialParam ?? value[0];
+  const hash = useMemo(() => SHA1(value).toString(), [value]);
+  const values = useMemo(() => hash.split('').map((it) => Number.parseInt(it, 16)), [hash]);
+
+  return (
+    <Container data-testid={buildTestId()}>
+      <Svg viewBox="0 0 15 15" fill="red">
+        <Square values={values} startIndex={0} color={theme.honeycomb.color.primary.normal} />
+        <Square values={values} startIndex={4} color={theme.honeycomb.color.success.normal} />
+        <Square values={values} startIndex={8} color={theme.honeycomb.color.danger.normal} />
+      </Svg>
+      <TextContainer>{initial}</TextContainer>
+    </Container>
+  );
+};
+
+Component.displayName = 'AccountAvatar';
---
+++ src/components/AccountAvatar/styled.tsx
@@ -0,0 +1,33 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  position: relative;
+  width: 1em;
+  height: 1em;
+  pointer-events: none;
+  user-select: none;
+  border-radius: 50%;
+  overflow: hidden;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const Svg = styled.svg`
+  width: 1em;
+  height: 1em;
+  background: ${({ theme }) => theme.honeycomb.color.bg.masked};
+`;
+
+export const TextContainer = styled.div`
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  top: 0;
+  left: 0;
+  font-size: 0.75em;
+  text-transform: uppercase;
+`;
