f2caca37fe20dc3e6243f3e73bc80ae55871a432
Rubén Illodo Brea <me@rubenillodo.com>
2020-05-29 12:39:34 +0200

    feat: With squares

--- src/components/AbstractShape/component.tsx
+++ src/components/AbstractShape/component.tsx
@@ -14,45 +14,78 @@ export const Component = ({ 'data-testid': testId, value }: Props) => {
   const values = useMemo(() => hash.split('').map((it) => Number.parseInt(it, 16)), [hash]);
 
   return (
-    <svg
-      data-testid={buildTestId()}
-      viewBox="0 0 15 15"
-      fill="red"
-      style={{
-        width: 200,
-        height: 200,
-        borderRadius: '50%',
-        background: `#${hash.substring(1, 7)}`,
-      }}
-    >
-      <path
-        d={`
-          M ${values[0]},${values[1]}
-          L ${values[2]},${values[3]}
-          L ${values[4]},${values[5]}
-          L ${values[6]},${values[7]}
-          L ${values[8]},${values[9]}
-          L ${values[10]},${values[11]}
-          L ${values[12]},${values[13]}
-          L ${values[0]},${values[1]}
-        `}
-        fill={`#${hash.substring(0, 6)}`}
-      />
-      <path
-        d={`
-          M ${values[14]},${values[15]}
-          L ${values[16]},${values[17]}
-          L ${values[18]},${values[19]}
-          L ${values[20]},${values[21]}
-          L ${values[22]},${values[23]}
-          L ${values[24]},${values[25]}
-          L ${values[26]},${values[27]}
-          L ${values[14]},${values[15]}
-        `}
-        fill={`#${hash.substring(6, 12)}`}
-      />
-      <circle cx="100" cy="100" r="100" clip-path="url(#cut-off-bottom)" />
-    </svg>
+    <div style={{ position: 'relative', width: '1em', height: '1em', fontSize: 200 }}>
+      <svg
+        data-testid={buildTestId()}
+        viewBox="0 0 15 15"
+        fill="red"
+        style={{
+          width: '1em',
+          height: '1em',
+          borderRadius: '50%',
+          pointerEvents: 'none',
+          userSelect: 'none',
+          background: `#${hash.substring(1, 7)}`,
+        }}
+      >
+        <g
+          transform={`rotate(${(values[3] / 16) * 360} ${(values[0] + values[2]) / 2} ${(values[1] +
+            values[2]) /
+            2})`}
+        >
+          <rect
+            x={values[0]}
+            y={values[1]}
+            width={values[2]}
+            height={values[2]}
+            fill={`#${hash.substring(0, 6)}`}
+          />
+        </g>
+        <g
+          transform={`rotate(${(values[7] / 16) * 360} ${(values[4] + values[6]) / 2} ${(values[5] +
+            values[6]) /
+            2})`}
+        >
+          <rect
+            x={values[4]}
+            y={values[5]}
+            width={values[6]}
+            height={values[6]}
+            fill={`#${hash.substring(6, 12)}`}
+          />
+        </g>
+        <g
+          transform={`rotate(${(values[11] / 16) * 360} ${(values[8] + values[10]) /
+            2} ${(values[9] + values[10]) / 2})`}
+        >
+          <rect
+            x={values[8]}
+            y={values[9]}
+            width={values[10]}
+            height={values[10]}
+            fill={`#${hash.substring(12, 18)}`}
+          />
+        </g>
+      </svg>
+      <div
+        style={{
+          position: 'absolute',
+          width: '100%',
+          height: '100%',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          top: 0,
+          left: 0,
+          fontSize: '0.75em',
+          pointerEvents: 'none',
+          userSelect: 'none',
+          WebkitTextStroke: '1px black',
+        }}
+      >
+        账
+      </div>
+    </div>
   );
 };
 
