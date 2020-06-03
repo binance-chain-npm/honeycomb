6eae816b3c03906dc665f9273f86fcba02a7396d
Rubén Illodo Brea <me@rubenillodo.com>
2020-06-03 18:03:18 +0200

    feat: Testable `Loading` component

--- src/components/Loading/component.tsx
+++ src/components/Loading/component.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
 
+import { Testable } from '../../modules/test-ids';
+
 import { Spinner, Rect1, Rect2, Rect3, Rect4 } from './styled';
 
-export interface Props {
+export type Props = Testable & {
   fillViewport?: boolean;
   className?: string;
-}
+};
 
-export const Component = (props: Props) => {
+export const Component = ({ fillViewport, className, 'data-testid': testId }: Props) => {
   return (
-    <Spinner fillViewport={props.fillViewport} className={props.className}>
+    <Spinner fillViewport={fillViewport} className={className} data-testid={testId}>
       <Rect1 />
       <Rect2 />
       <Rect3 />
