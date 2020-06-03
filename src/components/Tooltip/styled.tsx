390c10e3b46267f52d6fc6c30dc5fef7cb691580
Denis Stepanov <denis.stepanov@binance.com>
2020-06-03 09:36:57 +0300

    style: fix dropdown styles

--- src/components/Tooltip/styled.tsx
+++ src/components/Tooltip/styled.tsx
@@ -3,18 +3,19 @@ import { createGlobalStyle as css } from 'styled-components';
 import { hcStyle } from '../../modules/themes';
 
 export const Styles = css`
-  .tippy-tooltip {
+  .tippy-box {
+    min-width: 12.5em;
     background: ${({ theme }) => theme.honeycomb.color.bg.tooltip.normal};
     color: ${({ theme }) =>
       theme.honeycomb.color.readable.normal(theme.honeycomb.color.bg.tooltip.normal)};
     border-radius: ${hcStyle.radiusNormal()};
     box-shadow: rgba(0, 0, 0, 0.2) 0px 2px 1px -1px, rgba(0, 0, 0, 0.14) 0px 1px 1px 0px,
       rgba(0, 0, 0, 0.12) 0px 1px 3px 0px;
+    font-size: ${hcStyle.normal()};
+    overflow: hidden;
   }
 
-  .bare-theme {
-    .tippy-content {
-      padding: 0;
-    }
+  .tippy-content {
+    padding: 0;
   }
 `;
