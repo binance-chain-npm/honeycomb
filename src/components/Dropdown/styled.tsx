f72c1c51049141c461c51120547c8701456f70b3
Rubén Illodo Brea <me@rubenillodo.com>
2020-06-03 09:01:57 +0200

    fix: Adjust tooltip and dropdown styles

--- src/components/Dropdown/styled.tsx
+++ src/components/Dropdown/styled.tsx
@@ -1,17 +1,10 @@
 import styled, { createGlobalStyle as css } from 'styled-components';
-import { em } from 'polished';
+
+import { hcStyle } from '../../modules/themes';
 
 export const Styles = css`
-  .dropdown-theme {
-    &.tippy-tooltip {
-      background: ${({ theme }) => theme.honeycomb.color.bg.tooltip.normal};
-      color: ${({ theme }) =>
-        theme.honeycomb.color.readable.normal(theme.honeycomb.color.bg.tooltip.normal)};
-      min-width: ${em(200)};
-      font-size: 1em;
-      pointer-events: all;
-      overflow: hidden;
-    }
+  .tippy-box[data-theme='dropdown'] {
+    min-width: ${hcStyle.modularScale(21)({ forFontSize: 'reduced' })};
 
     .tippy-content {
       padding: 0;
