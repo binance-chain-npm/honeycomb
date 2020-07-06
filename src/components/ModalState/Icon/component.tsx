487a4cf1e20597a0af07ecccdd425baa404ca2d1
slwzero <slwzero2498@gmail.com>
2020-07-06 19:05:41 +0800

    ModalState Icon

--- src/components/ModalState/Icon/component.tsx
+++ src/components/ModalState/Icon/component.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import { Testable } from '../../../modules/test-ids';
 import { Variant } from '../styled';
 
-import { DialogSuccess, DialogWarning, DialogDanger } from './styled';
+import { DialogSuccess, DialogWarning, DialogDanger, DialogIcon } from './styled';
 
 export type Props = Testable & {
   variant: Variant;
@@ -17,7 +17,7 @@ const icons: any = {
 };
 
 export const Component = ({ variant, icon }: Props) => {
-  return <>{icon ? icon : icons[variant]}</>;
+  return <DialogIcon>{icon ? icon : icons[variant]}</DialogIcon>;
 };
 
 Component.displayName = 'ModalState.Icon';
--- src/components/ModalState/Icon/styled.tsx
+++ src/components/ModalState/Icon/styled.tsx
@@ -3,23 +3,15 @@ import { em } from 'polished';
 
 import { Icon } from '../../Icon';
 
-export const DialogSuccess = styled(Icon.DialogSuccess)`
-  font-size: ${({ theme }) => em(theme.honeycomb.size.normal)};
-  width: ${({ theme }) => em(theme.honeycomb.size.normal * 4)};
-  height: ${({ theme }) => em(theme.honeycomb.size.normal * 4)};
+export const DialogIcon = styled.div`
+  font-size: ${({ theme }) => em(theme.honeycomb.size.normal * 4)};
+  width: ${({ theme }) => em(theme.honeycomb.size.normal)};
+  height: ${({ theme }) => em(theme.honeycomb.size.normal)};
   align-self: center;
 `;
 
-export const DialogWarning = styled(Icon.DialogWarning)`
-  font-size: ${({ theme }) => em(theme.honeycomb.size.normal)};
-  width: ${({ theme }) => em(theme.honeycomb.size.normal * 4)};
-  height: ${({ theme }) => em(theme.honeycomb.size.normal * 4)};
-  align-self: center;
-`;
+export const DialogSuccess = styled(Icon.DialogSuccess)``;
 
-export const DialogDanger = styled(Icon.DialogDanger)`
-  font-size: ${({ theme }) => em(theme.honeycomb.size.normal)};
-  width: ${({ theme }) => em(theme.honeycomb.size.normal * 4)};
-  height: ${({ theme }) => em(theme.honeycomb.size.normal * 4)};
-  align-self: center;
-`;
+export const DialogWarning = styled(Icon.DialogWarning)``;
+
+export const DialogDanger = styled(Icon.DialogDanger)``;
