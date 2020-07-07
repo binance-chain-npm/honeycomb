10405f6b251af9484257e34a643ef39d68dbf930
Rubén Illodo Brea <me@rubenillodo.com>
2020-07-07 14:17:46 +0200

    fix: Unnecessary import

--- src/components/ModalPickOne/component.tsx
+++ src/components/ModalPickOne/component.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useCallback, useMemo } from 'react';
 
 import { Modal } from '../Modal';
-import { Testable, useBuildTestId } from '../../modules/test-ids';
+import { useBuildTestId } from '../../modules/test-ids';
 import { TextInput } from '../TextInput';
 import { Space } from '../Space';
 
