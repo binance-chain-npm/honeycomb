b7c8dc363dd203e19ff92a5ca8ea3d3920d93ae7
Denis Stepanov <denis.stepanov@binance.com>
2020-07-27 16:03:19 +0300

    feat: dropdown isSelected

--- src/components/Dropdown/Item/component.tsx
+++ src/components/Dropdown/Item/component.tsx
@@ -1,14 +1,17 @@
 import React, { useContext } from 'react';
+import { useTheme } from 'styled-components';
 
 import { TestIdContext } from '../context';
 import { useBuildTestId, Testable } from '../../../modules/test-ids';
 import { HtmlTag } from '../../../modules/html-tag';
+import { Icon } from '../../Icon';
 
 import { Container, Variant } from './styled';
 
 export type Props = React.ButtonHTMLAttributes<HTMLButtonElement> &
   React.AnchorHTMLAttributes<HTMLAnchorElement> &
   Testable & {
+    isSelected?: boolean;
     isNonInteractive?: boolean;
     variant?: Variant;
     htmlTag?: HtmlTag;
@@ -18,20 +21,24 @@ export const Component = ({
   htmlTag = 'button',
   variant = 'normal',
   isNonInteractive = false,
+  isSelected = false,
   ...otherProps
 }: Props) => {
+  const theme = useTheme();
   const parentTestId = useContext(TestIdContext);
   const buildTestId = useBuildTestId(otherProps['data-testid'] ? parentTestId : undefined);
 
   return (
     <Container
       {...otherProps}
+      isSelected={isSelected}
       isNonInteractive={isNonInteractive}
       variant={variant}
       as={htmlTag as any}
       data-testid={buildTestId(otherProps['data-testid'])}
     >
       {otherProps.children}
+      {isSelected && <Icon.Tick color={theme.honeycomb.color.primary.normal} />}
     </Container>
   );
 };
