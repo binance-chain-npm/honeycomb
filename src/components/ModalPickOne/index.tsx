c7148a0a27e608d6e51a2a7e6d2496b2a075c2ac
Jessica Xiao <jessicaoaix@gmail.com>
2020-07-30 02:27:00 +1200

    fix: Linting

--- src/components/ModalPickOne/index.tsx
+++ src/components/ModalPickOne/index.tsx
@@ -1,5 +1,6 @@
-import { Component } from './component';
 import { Item } from '../internal/PickOne/Item';
+
+import { Component } from './component';
 
 export const ModalPickOne = Component as typeof Component & { Item: typeof Item };
 
