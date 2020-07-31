541a6b6d7c2624659af46be8c86535484e035f1b
Jessica Xiao <jessicaoaix@gmail.com>
2020-07-31 18:47:35 +1200

    feat: Use new size from theme

--- src/components/TextInput/styled.tsx
+++ src/components/TextInput/styled.tsx
@@ -18,7 +18,7 @@ export const Description = styled.span`
   display: block;
   color: ${({ theme }) => theme.honeycomb.color.text.masked};
   font-size: ${({ theme }) => em(theme.honeycomb.size.small)};
-  margin-top: ${({ theme }) => em(4, theme.honeycomb.size.small)};
+  margin-top: ${({ theme }) => em(theme.honeycomb.size.micro, theme.honeycomb.size.small)};
 `;
 
 const success = css`
