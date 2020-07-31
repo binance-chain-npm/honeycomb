edf2288693e93fb57cf7618e492260c0ada8128f
Rubén Illodo Brea <me@rubenillodo.com>
2020-07-31 09:24:12 +0200

    test: All icon variants

--- src/components/ModalState/component.stories.tsx
+++ src/components/ModalState/component.stories.tsx
@@ -15,11 +15,35 @@ const scenario = {
   children: null as React.ReactNode,
 };
 
-export const Default = () => (
+export const Success = () => (
+  <ModalState
+    open={true}
+    data-testid="MyModal"
+    icon={<ModalState.Icon.Success />}
+    title={scenario.name}
+    description={scenario.description}
+  >
+    {scenario.children}
+  </ModalState>
+);
+
+export const Warning = () => (
   <ModalState
     open={true}
     data-testid="MyModal"
     icon={<ModalState.Icon.Warning />}
+    title={scenario.name}
+    description={scenario.description}
+  >
+    {scenario.children}
+  </ModalState>
+);
+
+export const Danger = () => (
+  <ModalState
+    open={true}
+    data-testid="MyModal"
+    icon={<ModalState.Icon.Danger />}
     title={scenario.name}
     description={scenario.description}
   >
