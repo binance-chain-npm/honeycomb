b4059fe1c3b5ad03f01f0a57b17da213fc48b38a
Rubén Illodo Brea <me@rubenillodo.com>
2020-08-04 13:53:43 +0200

    fix: Export `BigText` and `SmallText` so that styles can be overwritten

--- src/components/Balance/index.tsx
+++ src/components/Balance/index.tsx
@@ -1,1 +1,10 @@
-export { Component as Balance } from './component';
+import { Component } from './component';
+import { BigText, SmallText } from './styled';
+
+export const Balance = Component as typeof Component & {
+  BigText: typeof BigText;
+  SmallText: typeof SmallText;
+};
+
+Balance.BigText = BigText;
+Balance.SmallText = SmallText;
