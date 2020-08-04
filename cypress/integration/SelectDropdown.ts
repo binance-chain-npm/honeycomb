da4b0420e2f11697ce2c971ebf0a041acbfaa577
Jessica Xiao <jessicaoaix@gmail.com>
2020-08-04 15:11:19 +1200

    fix: Make Percy snapshots relevant

--- cypress/integration/SelectDropdown.ts
+++ cypress/integration/SelectDropdown.ts
@@ -1,6 +1,5 @@
 describe('Select variant="dropdown"', () => {
   it('displays modal on small devices', () => {
-    cy.viewport(400, 600);
     cy.visitStory({ storyId: 'elements-select--dropdown', themeId: 'GoldLight' });
 
     cy.get('[data-testid="select"]').click();
@@ -11,6 +10,8 @@ describe('Select variant="dropdown"', () => {
     cy.get('[data-testid="select.dropdown.modal.item.0"]').should('be.visible');
     cy.get('[data-testid="select.dropdown.modal.item.1"]').should('be.visible');
     cy.get('[data-testid="select.dropdown.modal.item.2"]').should('be.visible');
+
+    cy.percySnapshot('SelectDropdown while open on a small device');
   });
 
   it('displays dropdown on large devices', () => {
@@ -21,21 +22,7 @@ describe('Select variant="dropdown"', () => {
 
     cy.get('[data-testid="select.dropdown.modal.content"]').should('not.exist');
     cy.get('[data-testid="select.dropdown.dropdown.content"]').should('be.visible');
-  });
 
-  it('renders correctly with Gold Dark theme', () => {
-    cy.visitStory({ storyId: 'elements-select--dropdown', themeId: 'GoldDark' });
-    cy.clock();
-    cy.get('[data-testid="select"]').should('be.visible');
-    cy.tick(10000);
-    cy.percySnapshot('select.dropdown with Gold Dark theme');
-  });
-
-  it('renders correctly with Gold Light theme', () => {
-    cy.visitStory({ storyId: 'elements-select--dropdown', themeId: 'GoldLight' });
-    cy.clock();
-    cy.get('[data-testid="select"]').should('be.visible');
-    cy.tick(10000);
-    cy.percySnapshot('select.dropdown with Gold Light theme', { widths: [800] });
+    cy.percySnapshot('SelectDropdown while open on a large device', { widths: [800] });
   });
 });
--- cypress/integration/SelectModal.ts
+++ cypress/integration/SelectModal.ts
@@ -28,24 +28,7 @@ describe('Select variant="modal"', () => {
     cy.get('[data-testid="open-btn"]').click();
     cy.get('[data-testid="select.modal.box"]').should('be.visible');
     cy.get('[data-testid="select.modal.item.photo.tick"]').should('be.visible');
-  });
 
-  it('renders correctly with Gold Dark theme', () => {
-    cy.visitStory({ storyId: 'elements-select--modal', themeId: 'GoldDark' });
-    cy.get('[data-testid="open-btn"]').click();
-    cy.clock();
-    cy.get('[data-testid="select.modal.item.0.tick"]').should('be.visible');
-    cy.tick(10000);
-    cy.percySnapshot('ModalSelect with Gold Dark theme');
-  });
-
-  it('renders correctly with Gold Light theme', () => {
-    cy.viewport(800, 600);
-    cy.visitStory({ storyId: 'elements-select--modal', themeId: 'GoldLight' });
-    cy.get('[data-testid="open-btn"]').click();
-    cy.clock();
-    cy.get('[data-testid="select.modal.item.0.tick"]').should('be.visible');
-    cy.tick(10000);
-    cy.percySnapshot('ModalSelect with Gold Light theme', { widths: [800] });
+    cy.percySnapshot('SelectModal while open with option selected');
   });
 });
