1ea0542e3ef052618d50ceecbb0bf185c60c6a56
Rubén Illodo Brea <me@rubenillodo.com>
2020-08-15 17:56:40 +0200

    fix: Dropdown width

--- src/components/Dropdown/component.tsx
+++ src/components/Dropdown/component.tsx
@@ -4,7 +4,7 @@ import { Testable, useBuildTestId } from '../../modules/test-ids';
 import { Tooltip } from '../Tooltip';
 import { Styleless } from '../Styleless';
 
-import { Styles } from './styled';
+import { TooltipContent } from './styled';
 import { ShowingContext, TestIdContext } from './context';
 
 export type TriggerValue = 'mouseenter' | 'focus' | 'click';
@@ -22,27 +22,24 @@ export const Component = (props: Props) => {
   const hide = useCallback(() => setShowing(false), [setShowing]);
 
   return (
-    <>
-      <Styles />
-      <Tooltip
-        className={props.className}
-        trigger="click"
-        interactive={true}
-        onShow={show}
-        onHide={hide}
-        content={
-          <TestIdContext.Provider value={buildTestId('content')}>
-            {props.children}
-          </TestIdContext.Provider>
-        }
-        data-testid={props['data-testid']}
-        arrow={false}
-      >
-        <Styleless htmlTag="div">
-          <ShowingContext.Provider value={isShowing}>{props.target}</ShowingContext.Provider>
-        </Styleless>
-      </Tooltip>
-    </>
+    <Tooltip
+      className={props.className}
+      trigger="click"
+      interactive={true}
+      onShow={show}
+      onHide={hide}
+      content={
+        <TestIdContext.Provider value={buildTestId('content')}>
+          <TooltipContent>{props.children}</TooltipContent>
+        </TestIdContext.Provider>
+      }
+      data-testid={props['data-testid']}
+      arrow={false}
+    >
+      <Styleless htmlTag="div">
+        <ShowingContext.Provider value={isShowing}>{props.target}</ShowingContext.Provider>
+      </Styleless>
+    </Tooltip>
   );
 };
 
