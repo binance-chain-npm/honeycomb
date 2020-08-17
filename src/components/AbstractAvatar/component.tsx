f39455f9f1dfd85e1c24d925927c2abb863e890c
Rubén Illodo Brea <me@rubenillodo.com>
2020-08-17 11:58:48 +0200

    feat: Improved `AbstractAvatar` and `SolidAvatar` (#93)

--- src/components/AbstractAvatar/component.tsx
+++ src/components/AbstractAvatar/component.tsx
@@ -1,8 +1,8 @@
 import React, { useMemo } from 'react';
 import { SHA1 } from 'crypto-js';
-import { useTheme } from 'styled-components';
 
 import { Testable, useBuildTestId } from '../../modules/test-ids';
+import { ABSTRACT_AVATAR_COLORS } from '../internal/avatar-colors';
 
 import { Svg, TextContainer, Container } from './styled';
 import { Square } from './Square';
@@ -14,17 +14,23 @@ export type Props = Testable & {
 };
 
 export const Component = ({ 'data-testid': testId, value, initial, className }: Props) => {
-  const theme = useTheme();
   const buildTestId = useBuildTestId(testId);
   const hash = useMemo(() => SHA1(value).toString(), [value]);
   const values = useMemo(() => hash.split('').map((it) => Number.parseInt(it, 16)), [hash]);
+  const bgColor = useMemo(() => {
+    const index = Number.parseInt(hash.split('')[0], 16) % ABSTRACT_AVATAR_COLORS.length;
+    return ABSTRACT_AVATAR_COLORS[index];
+  }, [hash]);
 
   return (
     <Container className={className} data-testid={buildTestId()}>
-      <Svg viewBox="0 0 15 15" fill="red">
-        <Square values={values} startIndex={0} color={theme.honeycomb.color.primary.normal} />
-        <Square values={values} startIndex={4} color={theme.honeycomb.color.success.normal} />
-        <Square values={values} startIndex={8} color={theme.honeycomb.color.danger.normal} />
+      <Svg viewBox="0 0 15 15">
+        <rect x="0" y="0" width="15" height="15" fill={bgColor} />
+        <Square values={values} startIndex={0} />
+        <Square values={values} startIndex={4} />
+        <Square values={values} startIndex={8} />
+        <Square values={values} startIndex={12} />
+        <Square values={values} startIndex={16} />
       </Svg>
       {!!initial && <TextContainer>{initial[0]}</TextContainer>}
     </Container>
