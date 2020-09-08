b24d145f13cb6f0183795f16623dfe65db41645f
Jessica Xiao <jessicaoaix@gmail.com>
2020-09-08 21:41:00 +1200

    feat: Remove duplicate test and just add Percy snapshot at multiple widths

--- cypress/integration/DynamicTextInput.ts
+++ cypress/integration/DynamicTextInput.ts
@@ -1,20 +1,10 @@
 describe('DynamicTextInput', () => {
-  it('renders properly with Gold Light theme', () => {
+  it('renders correctly', () => {
     cy.visitStory({ storyId: 'inputs-dynamictextinput--default', themeId: 'GoldLight' });
 
     cy.get('[data-testid="input"]').clear();
     cy.get('[data-testid="input"]').type('Some really really really long text...');
 
-    cy.percySnapshot('DynamicTextInput with Gold Light theme');
-  });
-
-  it('renders properly with Gold Dark theme', () => {
-    cy.viewport(800, 600);
-    cy.visitStory({ storyId: 'inputs-dynamictextinput--default', themeId: 'GoldDark' });
-
-    cy.get('[data-testid="input"]').clear();
-    cy.get('[data-testid="input"]').type('Some really really really long text...');
-
-    cy.percySnapshot('DynamicTextInput with Gold Dark theme', { widths: [800] });
+    cy.percySnapshot('DynamicTextInput', { widths: [375, 800] });
   });
 });
