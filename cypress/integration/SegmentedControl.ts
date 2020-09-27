d223a2cad9e234a7010fcc7d376b396ba44699b0
zj <xzjcool@live.com>
2020-09-27 23:06:49 +0800

    fix: update integration test

--- cypress/integration/SegmentedControl.ts
+++ cypress/integration/SegmentedControl.ts
@@ -2,36 +2,36 @@ describe('SegmentedControl', () => {
   it('<SegmentedControl variant="button" /> renders correctly', () => {
     cy.visitStory({ storyId: 'elements-control--segmented-control', themeId: 'GoldLight' });
 
-    cy.get('[data-testid="segmented-control.huge"]').should('be.visible');
-    cy.get('[data-testid="segmented-control.huge.0"]').should(
+    cy.get('[data-testid="buttons-control.huge"]').should('be.visible');
+    cy.get('[data-testid="buttons-control.huge.0"]').should(
       'have.attr',
       'data-testisselected',
       'true',
     );
-    cy.get('[data-testid="segmented-control.huge.1"]').should(
+    cy.get('[data-testid="buttons-control.huge.1"]').should(
       'have.attr',
       'data-testisselected',
       'false',
     );
-    cy.get('[data-testid="segmented-control.huge.2"]').should(
+    cy.get('[data-testid="buttons-control.huge.2"]').should(
       'have.attr',
       'data-testisselected',
       'false',
     );
     cy.percySnapshot('<SegmentedControl variant="button" />');
 
-    cy.get('[data-testid="segmented-control.huge.1"]').click();
-    cy.get('[data-testid="segmented-control.huge.0"]').should(
+    cy.get('[data-testid="buttons-control.huge.1"]').click();
+    cy.get('[data-testid="buttons-control.huge.0"]').should(
       'have.attr',
       'data-testisselected',
       'false',
     );
-    cy.get('[data-testid="segmented-control.huge.1"]').should(
+    cy.get('[data-testid="buttons-control.huge.1"]').should(
       'have.attr',
       'data-testisselected',
       'true',
     );
-    cy.get('[data-testid="segmented-control.huge.2"]').should(
+    cy.get('[data-testid="buttons-control.huge.2"]').should(
       'have.attr',
       'data-testisselected',
       'false',
