a032c540749aff21a826e98eac9af4fb850e4024
Jessica Xiao <jessicaoaix@gmail.com>
2020-09-29 00:43:43 +1300

    feat: Show background on header component story

--- src/components/Header/component.stories.tsx
+++ src/components/Header/component.stories.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import { Sections } from '../../modules/sections';
+import { Card } from '../Card';
 import { Icon } from '../Icon';
 
 import { Header } from '.';
@@ -13,31 +14,33 @@ export default {
 const dropdown = (
   <Header.Dropdown target="Dropdown Item">
     <Header.Dropdown.Item>Item 1</Header.Dropdown.Item>
-    <Header.Dropdown.Item htmlTag="a">Item 2</Header.Dropdown.Item>
+    <Header.Dropdown.Item>Item 2</Header.Dropdown.Item>
     <Header.Dropdown.Divider />
-    <Header.Dropdown.Item variant="accent">Item 3</Header.Dropdown.Item>
+    <Header.Dropdown.Item>Item 3</Header.Dropdown.Item>
   </Header.Dropdown>
 );
 
 export const Default = () => (
-  <Header
-    logo={
-      <>
-        <Icon.BinanceChain />
-        &nbsp;Binance Chain
-      </>
-    }
-    left={
-      <>
-        <Header.Item>Left Item</Header.Item>
-        {dropdown}
-      </>
-    }
-    right={
-      <>
-        <Header.Item>Right Item</Header.Item>
-        {dropdown}
-      </>
-    }
-  />
+  <Card variant="bare">
+    <Header
+      logo={
+        <>
+          <Icon.BinanceChain />
+          &nbsp;Binance Chain
+        </>
+      }
+      left={
+        <>
+          <Header.Item>Left Item</Header.Item>
+          {dropdown}
+        </>
+      }
+      right={
+        <>
+          <Header.Item>Right Item</Header.Item>
+          {dropdown}
+        </>
+      }
+    />
+  </Card>
 );
