73913b3eed138c836b16d079ea3a630eac3fe895
Jessica Xiao <jessicaoaix@gmail.com>
2020-10-05 15:14:09 +1300

    feat: Export icon name

--- src/components/Icon/urls.tsx
+++ src/components/Icon/urls.tsx
@@ -44,6 +44,7 @@ export { default as Success } from './assets/Success.svg';
 export { default as TestnetFaucet } from './assets/TestnetFaucet.svg';
 export { default as Tick } from './assets/Tick.svg';
 export { default as TriangleDown } from './assets/TriangleDown.svg';
+export { default as TriangleUp } from './assets/TriangleUp.svg';
 export { default as VisaColor } from './assets/VisaColor.svg';
 export { default as Wallet } from './assets/Wallet.svg';
 export { default as WalletConnect } from './assets/WalletConnect.svg';
