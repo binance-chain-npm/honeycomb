bec4d9333b392984e8dd5a13945c6067ee6afd20
Jessica Xiao <jessicaoaix@gmail.com>
2020-11-12 01:07:28 +1300

    fix(BuyWithMoonPay): Icon sizing

--- src/components/BuyWithMoonPay/styled.tsx
+++ src/components/BuyWithMoonPay/styled.tsx
@@ -4,10 +4,9 @@ import { em } from 'polished';
 import { Icon } from '../Icon';
 
 export const StyledVisa = styled(Icon.VisaColor)`
-  font-size: ${({ theme }) => em(8, theme.honeycomb.size.reduced)};
+  font-size: ${({ theme }) => em(theme.honeycomb.size.increased, theme.honeycomb.size.reduced)};
 `;
 
 export const StyledMastercard = styled(Icon.MastercardColor)`
-  font-size: ${({ theme }) => em(8, theme.honeycomb.size.reduced)};
-  margin-left: ${({ theme }) => em(4, theme.honeycomb.size.small)};
+  font-size: ${({ theme }) => em(theme.honeycomb.size.small, theme.honeycomb.size.reduced)};
 `;
