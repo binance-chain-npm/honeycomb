e7be26d063694dc965d401c4a7f6248ff7b8f2bf
Jessica Xiao <jessicaoaix@gmail.com>
2020-11-13 01:56:48 +1300

    fix: Minor test fixes

--- cypress/integration/Breadcrumbs.ts
+++ cypress/integration/Breadcrumbs.ts
@@ -1,6 +1,6 @@
 describe('Breadcrumbs', () => {
   it('works when clicking on label', () => {
-    cy.visitStory({ storyId: 'elements-breadcrumbs--default', themeId: 'GoldDark' });
+    cy.visitStory({ storyId: 'elements-breadcrumbs--default', themeId: 'GoldLight' });
 
     cy.get('[data-testid="light.breadcrumb.0"]').should('have.text', 'Page A');
     cy.get('[data-testid="light.breadcrumb.1"]').should('have.text', 'A.1');
--- cypress/integration/CopyToClipboard.ts
+++ cypress/integration/CopyToClipboard.ts
@@ -4,11 +4,16 @@ describe('CopyToClipboard', () => {
     cy.clock();
 
     cy.get('[data-testid="light.copy-to-clipboard.copy-icon"]').should('be.visible');
-    cy.percySnapshot('CopyToClipboard in Gold Light theme');
+
+    cy.percySnapshot('CopyToClipboard');
 
     cy.get('[data-testid="light.copy-to-clipboard"]').click();
     cy.tick(1000);
     cy.get('[data-testid="light.copy-to-clipboard.tick-icon"]').should('be.visible');
-    cy.percySnapshot('CopyToClipboard after clicking in Gold Light theme');
+  
+    cy.percySnapshot('CopyToClipboard after clicking');
+
+    cy.tick(10000);
+    cy.get('[data-testid="light.copy-to-clipboard.copy-icon"]').should('be.visible');
   });
 });
