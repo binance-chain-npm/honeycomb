cca61e24d00b7006b388fff675fdf318d078d3a0
Jessica Xiao <jessicaoaix@gmail.com>
2020-11-12 22:14:58 +1300

    fix(Breadcrumbs)

--- cypress/integration/Breadcrumbs.ts
+++ cypress/integration/Breadcrumbs.ts
@@ -1,33 +1,33 @@
 describe('Breadcrumbs', () => {
   it('works when clicking on label', () => {
-    cy.visitStory({ storyId: 'tests-breadcrumbs--default', themeId: 'GoldDark' });
+    cy.visitStory({ storyId: 'elements-breadcrumbs--default', themeId: 'GoldDark' });
 
-    cy.get('[data-testid="breadcrumb.0"]').should('have.text', 'Page A');
-    cy.get('[data-testid="breadcrumb.1"]').should('have.text', 'A.1');
+    cy.get('[data-testid="light.breadcrumb.0"]').should('have.text', 'Page A');
+    cy.get('[data-testid="light.breadcrumb.1"]').should('have.text', 'A.1');
 
-    cy.get('[data-testid="toggle.A1-A2"]').click();
+    cy.get('[data-testid="light.toggle.A1-A2"]').click();
 
-    cy.get('[data-testid="breadcrumb.0"]').should('have.text', 'Page A');
-    cy.get('[data-testid="breadcrumb.1"]').should('have.text', 'A.2');
+    cy.get('[data-testid="light.breadcrumb.0"]').should('have.text', 'Page A');
+    cy.get('[data-testid="light.breadcrumb.1"]').should('have.text', 'A.2');
 
-    cy.get('[data-testid="toggle.A1-A2"]').click();
+    cy.get('[data-testid="light.toggle.A1-A2"]').click();
 
-    cy.get('[data-testid="breadcrumb.0"]').should('have.text', 'Page A');
-    cy.get('[data-testid="breadcrumb.1"]').should('have.text', 'A.1');
+    cy.get('[data-testid="light.breadcrumb.0"]').should('have.text', 'Page A');
+    cy.get('[data-testid="light.breadcrumb.1"]').should('have.text', 'A.1');
 
-    cy.get('[data-testid="toggle.A-B"]').click();
+    cy.get('[data-testid="light.toggle.A-B"]').click();
 
-    cy.get('[data-testid="breadcrumb.0"]').should('have.text', 'Page B');
-    cy.get('[data-testid="breadcrumb.1"]').should('not.exist');
+    cy.get('[data-testid="light.breadcrumb.0"]').should('have.text', 'Page B');
+    cy.get('[data-testid="light.breadcrumb.1"]').should('not.exist');
 
-    cy.get('[data-testid="toggle.A-B"]').click();
+    cy.get('[data-testid="light.toggle.A-B"]').click();
 
-    cy.get('[data-testid="breadcrumb.0"]').should('have.text', 'Page A');
-    cy.get('[data-testid="breadcrumb.1"]').should('have.text', 'A.1');
+    cy.get('[data-testid="light.breadcrumb.0"]').should('have.text', 'Page A');
+    cy.get('[data-testid="light.breadcrumb.1"]').should('have.text', 'A.1');
 
-    cy.get('[data-testid="toggle.A-B"]').click();
+    cy.get('[data-testid="light.toggle.A-B"]').click();
 
-    cy.get('[data-testid="breadcrumb.0"]').should('have.text', 'Page B');
-    cy.get('[data-testid="breadcrumb.1"]').should('not.exist');
+    cy.get('[data-testid="light.breadcrumb.0"]').should('have.text', 'Page B');
+    cy.get('[data-testid="light.breadcrumb.1"]').should('not.exist');
   });
 });
