438e9c0614c820f6ba24992ea8ff83575a1e2c7d
Jessica Xiao <jessicaoaix@gmail.com>
2020-11-13 00:28:54 +1300

    fix(Loading): Hacky way to not show all themes so components don't stack

--- src/components/Loading/component.stories.tsx
+++ src/components/Loading/component.stories.tsx
@@ -1,19 +1,30 @@
 import React from 'react';
 
 import { Sections } from '../../modules/sections';
+import { useBuildTestId } from '../../modules/test-ids';
 import { Button } from '../Button';
 
 import { Loading } from './';
 
 export default {
+  component: Loading,
   title: `${Sections.Elements}/Loading`,
 };
 
 export const Default = () => <Loading />;
 
-export const FillViewport = () => (
-  <>
-    <Button variant="primary">A test</Button>
-    <Loading fillViewport />
-  </>
-);
+export const FillViewport = () => {
+  const { buildTestId } = useBuildTestId();
+
+  const id = buildTestId();
+
+  return (
+    id &&
+    id.startsWith('light') && (
+      <>
+        <Button variant="primary">Button</Button>
+        <Loading fillViewport />
+      </>
+    )
+  );
+};
