cb51d98b913f45c0763bf9a1ce31b89f0100c52a
Jessica Xiao <jessicaoaix@gmail.com>
2020-11-12 23:38:22 +1300

    fix(Header): Fix logo not correctly coloured on accent theme, fix tests

--- cypress/integration/Header.ts
+++ cypress/integration/Header.ts
@@ -5,19 +5,19 @@ describe('Header', () => {
     cy.visitStory({ storyId: 'elements-header--with-complex-items', themeId: 'GoldLight' });
     cy.tick(10000);
 
-    cy.get('[data-testid="header.logo.logo"]').should('be.visible');
-    cy.get('[data-testid="header.logo.badge"]').should('be.visible');
+    cy.get('[data-testid="light.header.logo.logo"]').should('be.visible');
+    cy.get('[data-testid="light.header.logo.badge"]').should('be.visible');
 
-    cy.get('[data-testid="header.left"]').should('be.visible');
-    cy.get('[data-testid="header.right"]').should('be.visible');
-    cy.get('[data-testid="header.non-collapsible"]').should('be.visible');
-    cy.get('[data-testid="header.menu"]').should('not.exist');
-    cy.get('[data-testid="header.drawer.container"]').should('not.be.visible');
+    cy.get('[data-testid="light.header.left"]').should('be.visible');
+    cy.get('[data-testid="light.header.right"]').should('be.visible');
+    cy.get('[data-testid="light.header.non-collapsible"]').should('be.visible');
+    cy.get('[data-testid="light.header.menu"]').should('not.exist');
+    cy.get('[data-testid="light.header.drawer.container"]').should('not.be.visible');
 
-    cy.get('[data-testid="header.left.dropdown.target"]').click();
+    cy.get('[data-testid="light.header.left.dropdown.target"]').click();
     cy.tick(10000);
 
-    cy.get('[data-testid="header.left.dropdown.content"]').should('be.visible');
+    cy.get('[data-testid="light.header.left.dropdown.content"]').should('be.visible');
 
     cy.percySnapshot('Header while open on a large device', { widths: [1280] });
   });
@@ -28,23 +28,23 @@ describe('Header', () => {
     cy.visitStory({ storyId: 'elements-header--with-complex-items', themeId: 'GoldLight' });
     cy.tick(10000);
 
-    cy.get('[data-testid="header.logo.logo"]').should('be.visible');
-    cy.get('[data-testid="header.logo.badge"]').should('be.visible');
+    cy.get('[data-testid="light.header.logo.logo"]').should('be.visible');
+    cy.get('[data-testid="light.header.logo.badge"]').should('be.visible');
 
-    cy.get('[data-testid="header.left"]').should('be.visible');
-    cy.get('[data-testid="header.right"]').should('not.exist');
-    cy.get('[data-testid="header.non-collapsible"]').should('be.visible');
-    cy.get('[data-testid="header.menu"]').should('be.visible');
-    cy.get('[data-testid="header.drawer.container"]').should('not.be.visible');
+    cy.get('[data-testid="light.header.left"]').should('be.visible');
+    cy.get('[data-testid="light.header.right"]').should('not.exist');
+    cy.get('[data-testid="light.header.non-collapsible"]').should('be.visible');
+    cy.get('[data-testid="light.header.menu"]').should('be.visible');
+    cy.get('[data-testid="light.header.drawer.container"]').should('not.be.visible');
 
-    cy.get('[data-testid="header.menu"]').click();
+    cy.get('[data-testid="light.header.menu"]').click();
     cy.tick(10000);
 
-    cy.get('[data-testid="header.drawer.container"]').should('be.visible');
-    cy.get('[data-testid="header.drawer.content"]').should('be.visible');
-    cy.get('[data-testid="header.accordion"]').should('be.visible');
+    cy.get('[data-testid="light.header.drawer.container"]').should('be.visible');
+    cy.get('[data-testid="light.header.drawer.content"]').should('be.visible');
+    cy.get('[data-testid="light.header.accordion"]').should('be.visible');
     
-    cy.get('[data-testid="header.accordion"]')
+    cy.get('[data-testid="light.header.accordion"]')
       .children()
       .then((children) => {
         expect(children.length).to.equal(4);
@@ -59,24 +59,24 @@ describe('Header', () => {
     cy.visitStory({ storyId: 'elements-header--with-complex-items', themeId: 'GoldLight' });
     cy.tick(10000);
 
-    cy.get('[data-testid="header.logo.logo"]').should('not.exist');
-    cy.get('[data-testid="header.logo.logo-sm"]').should('be.visible');
-    cy.get('[data-testid="header.logo.badge"]').should('be.visible');
+    cy.get('[data-testid="light.header.logo.logo"]').should('not.exist');
+    cy.get('[data-testid="light.header.logo.logo-sm"]').should('be.visible');
+    cy.get('[data-testid="light.header.logo.badge"]').should('be.visible');
 
-    cy.get('[data-testid="header.left"]').should('not.exist');
-    cy.get('[data-testid="header.right"]').should('not.exist');
-    cy.get('[data-testid="header.non-collapsible"]').should('be.visible');
-    cy.get('[data-testid="header.menu"]').should('be.visible');
-    cy.get('[data-testid="header.drawer.container"]').should('not.be.visible');
+    cy.get('[data-testid="light.header.left"]').should('not.exist');
+    cy.get('[data-testid="light.header.right"]').should('not.exist');
+    cy.get('[data-testid="light.header.non-collapsible"]').should('be.visible');
+    cy.get('[data-testid="light.header.menu"]').should('be.visible');
+    cy.get('[data-testid="light.header.drawer.container"]').should('not.be.visible');
 
-    cy.get('[data-testid="header.menu"]').click();
+    cy.get('[data-testid="light.header.menu"]').click();
     cy.tick(10000);
 
-    cy.get('[data-testid="header.drawer.container"]').should('be.visible');
-    cy.get('[data-testid="header.drawer.content"]').should('be.visible');
-    cy.get('[data-testid="header.accordion"]').should('be.visible');
+    cy.get('[data-testid="light.header.drawer.container"]').should('be.visible');
+    cy.get('[data-testid="light.header.drawer.content"]').should('be.visible');
+    cy.get('[data-testid="light.header.accordion"]').should('be.visible');
     
-    cy.get('[data-testid="header.accordion"]')
+    cy.get('[data-testid="light.header.accordion"]')
       .children()
       .then((children) => {
         expect(children.length).to.equal(6);
@@ -89,9 +89,9 @@ describe('Header', () => {
     cy.customViewport({ size: 'lg' });
     cy.visitStory({ storyId: 'elements-header--with-complex-items', themeId: 'GoldLight' });
 
-    cy.get('[data-testid="header.left.dropdown.target"]').click();
+    cy.get('[data-testid="light.header.left.dropdown.target"]').click();
     
-    cy.get('[data-testid="header.left.dropdown.content"]')
+    cy.get('[data-testid="light.header.left.dropdown.content"]')
       .children()
       .children()
       .then((children) => {
@@ -102,10 +102,10 @@ describe('Header', () => {
   it('dropdown in drawer behaves correctly', () => {
     cy.visitStory({ storyId: 'elements-header--with-complex-items', themeId: 'GoldLight' });
 
-    cy.get('[data-testid="header.menu"]').click();
-    cy.get('[data-testid="header.accordion.1"]').click();
+    cy.get('[data-testid="light.header.menu"]').click();
+    cy.get('[data-testid="light.header.accordion.1"]').click();
     
-    cy.get('[data-testid="header.accordion.1.children"]')
+    cy.get('[data-testid="light.header.accordion.1.children"]')
       .children()
       .then((children) => {
         expect(children.length).to.equal(4);
@@ -113,63 +113,63 @@ describe('Header', () => {
   });
 
   it('clicking items in the drawer behaves correctly', () => {
-    cy.get('[data-testid="header.accordion.0"]').click();
+    cy.get('[data-testid="light.header.accordion.0"]').click();
 
-    cy.get('[data-testid="header.drawer.container"]').should('not.be.visible');
+    cy.get('[data-testid="light.header.drawer.container"]').should('not.be.visible');
 
-    cy.get('[data-testid="header.menu"]').click();
-    cy.get('[data-testid="header.accordion.0"]').click();
+    cy.get('[data-testid="light.header.menu"]').click();
+    cy.get('[data-testid="light.header.accordion.0"]').click();
 
-    cy.get('[data-testid="header.drawer.container"]').should('not.be.visible');
+    cy.get('[data-testid="light.header.drawer.container"]').should('not.be.visible');
   });
 
   it('non-collapsible items behave correctly', () => {
     cy.customViewport({ size: 'lg' });
     cy.visitStory({ storyId: 'elements-header--with-non-collapsible-items', themeId: 'GoldLight' });
 
-    cy.get('[data-testid="header.non-collapsible"]')
+    cy.get('[data-testid="light.header.non-collapsible"]')
       .children()
       .then((children) => {
-        expect(children[0]).to.have.attr('data-testid', 'non-collapsible');
-        expect(children[1]).to.have.attr('data-testid', 'non-collapsible.md');
-        expect(children[2]).to.have.attr('data-testid', 'non-collapsible.sm');
+        expect(children[0]).to.have.attr('data-testid', 'light.non-collapsible');
+        expect(children[1]).to.have.attr('data-testid', 'light.non-collapsible.md');
+        expect(children[2]).to.have.attr('data-testid', 'light.non-collapsible.sm');
       });
-    cy.get('[data-testid="header.menu"]').should('not.exist');
+    cy.get('[data-testid="light.header.menu"]').should('not.exist');
 
     cy.customViewport({ size: 'md' });
     cy.reload();
 
-    cy.get('[data-testid="header.non-collapsible"]')
+    cy.get('[data-testid="light.header.non-collapsible"]')
       .children()
       .then((children) => {
-        expect(children[0]).to.have.attr('data-testid', 'non-collapsible');
-        expect(children[1]).to.have.attr('data-testid', 'non-collapsible.sm');
+        expect(children[0]).to.have.attr('data-testid', 'light.non-collapsible');
+        expect(children[1]).to.have.attr('data-testid', 'light.non-collapsible.sm');
       });
-    cy.get('[data-testid="header.menu"]').click();
-    cy.get('[data-testid="header.accordion.1"]')
+    cy.get('[data-testid="light.header.menu"]').click();
+    cy.get('[data-testid="light.header.accordion.1"]')
       .children()
       .then((children) => {
-        expect(children[0]).to.have.attr('data-testid', 'non-collapsible.md');
+        expect(children[0]).to.have.attr('data-testid', 'light.non-collapsible.md');
       });
 
     cy.customViewport({ size: 'sm' });
     cy.reload();
 
-    cy.get('[data-testid="header.non-collapsible"]')
+    cy.get('[data-testid="light.header.non-collapsible"]')
       .children()
       .then((children) => {
-        expect(children[0]).to.have.attr('data-testid', 'non-collapsible');
+        expect(children[0]).to.have.attr('data-testid', 'light.non-collapsible');
       });
-    cy.get('[data-testid="header.menu"]').click();
-    cy.get('[data-testid="header.accordion.2"]')
+    cy.get('[data-testid="light.header.menu"]').click();
+    cy.get('[data-testid="light.header.accordion.2"]')
       .children()
       .then((children) => {
-        expect(children[0]).to.have.attr('data-testid', 'non-collapsible.md');
+        expect(children[0]).to.have.attr('data-testid', 'light.non-collapsible.md');
       });
-    cy.get('[data-testid="header.accordion.3"]')
+    cy.get('[data-testid="light.header.accordion.3"]')
       .children()
       .then((children) => {
-        expect(children[0]).to.have.attr('data-testid', 'non-collapsible.sm');
+        expect(children[0]).to.have.attr('data-testid', 'light.non-collapsible.sm');
       });
   });
 });
