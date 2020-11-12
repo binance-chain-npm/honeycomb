5600c8257dfb5423655c204e88ce4c1efe426b15
Jessica Xiao <jessicaoaix@gmail.com>
2020-11-13 03:20:59 +1300

    feat(Tooltip): Add snapshot for variants story

--- cypress/integration/Tooltip.ts
+++ cypress/integration/Tooltip.ts
@@ -1,8 +1,12 @@
 describe('Tooltip', () => {
   it('renders correctly', () => {
-    cy.visitStory({ storyId: 'elements-tooltip--mixed-themes', themeId: 'GoldLight' });
-
     cy.clock();
+    cy.visitStory({ storyId: 'elements-tooltip--mixed-themes', themeId: 'GoldLight' });
+    cy.tick(10000);
+
+    cy.percySnapshot('Tooltip with mixed themes');
+
+    cy.visitStory({ storyId: 'elements-tooltip--variants', themeId: 'GoldLight' });
     cy.tick(10000);
 
     cy.percySnapshot('Tooltip');
