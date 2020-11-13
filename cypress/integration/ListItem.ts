4de8e8364edb410b11b85e29a04cad5f10ccf0dd
Jessica Xiao <jessicaoaix@gmail.com>
2020-11-13 14:20:27 +1300

    feat(ListItem): Undo adding clock in test, Percy snapshot still wrong

--- cypress/integration/ListItem.ts
+++ cypress/integration/ListItem.ts
@@ -1,10 +1,6 @@
 describe('ListItem', () => {
   it('renders correctly', () => {
-    cy.clock();
-
     cy.visitStory({ storyId: 'elements-listitem--default', themeId: 'GoldLight' });
-    cy.tick(10000);
-
     cy.percySnapshot('ListItem');
   });
 });
