9f626c85da731b10cb26acc12c919570dc905201
Jessica Xiao <jessicaoaix@gmail.com>
2020-12-02 00:27:10 +1300

    fix: Spacing

--- src/components/Tooltip/component.stories.tsx
+++ src/components/Tooltip/component.stories.tsx
@@ -135,7 +135,7 @@ export const Variants = () => {
 };
 
 const FixedHeight = styled.div`
-  height: 3em;
+  height: 4em;
 `;
 
 export const MixedThemes = () => {
@@ -146,22 +146,19 @@ export const MixedThemes = () => {
           <div />
         </Tooltip>
       </HoneycombThemeProvider>
-      <FixedHeight />
       <HoneycombThemeProvider variant="dark">
         <Tooltip content="Dark theme." visible arrow={true} shape="fit">
-          <div />
+          <FixedHeight />
         </Tooltip>
       </HoneycombThemeProvider>
-      <FixedHeight />
       <HoneycombThemeProvider variant="light">
         <Tooltip content="Light accent theme." variant="accent" visible arrow={true} shape="fit">
-          <div />
+          <FixedHeight />
         </Tooltip>
       </HoneycombThemeProvider>
-      <FixedHeight />
       <HoneycombThemeProvider variant="dark">
         <Tooltip content="Dark accent theme." variant="accent" visible arrow={true} shape="fit">
-          <div />
+          <FixedHeight />
         </Tooltip>
       </HoneycombThemeProvider>
     </>
