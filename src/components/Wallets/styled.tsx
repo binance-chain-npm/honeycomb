004afe7a2ae0080f087500c53c14e864f43b9444
Jessica Xiao <jessicaoaix@gmail.com>
2021-02-12 00:05:21 +1300

    feat: Fix description layout on small screens

--- src/components/Wallets/styled.tsx
+++ src/components/Wallets/styled.tsx
@@ -7,12 +7,12 @@ import { PanelControl } from '../PanelControl';
 export const StyledPanelControl = styled(PanelControl)<{ columns: number }>`
   display: grid;
   grid-template-columns: minmax(0, 1fr);
-  grid-auto-rows: 1fr;
   row-gap: ${({ theme }) => em(theme.honeycomb.size.normal)};
 
   @media (min-width: ${em(SIZES.md)}) {
     grid-template-columns: repeat(${({ columns }) => columns}, minmax(0, 1fr));
     column-gap: ${({ theme }) => em(theme.honeycomb.size.normal)};
+    grid-auto-rows: 1fr;
   }
 `;
 
@@ -22,6 +22,7 @@ export const StyledPanelControlItem = styled(PanelControl.Item)`
     ${({ theme }) => em(theme.honeycomb.size.small, theme.honeycomb.size.reduced)};
   align-items: center;
   justify-content: space-between;
+  flex-flow: wrap;
   width: 100%;
   height: 100%;
 
@@ -48,4 +49,16 @@ export const Description = styled.div`
   color: ${({ theme }) => theme.honeycomb.color.text.masked};
   margin-top: ${({ theme }) => em(theme.honeycomb.size.tiny, theme.honeycomb.size.reduced)};
   font-size: ${({ theme }) => em(theme.honeycomb.size.small, theme.honeycomb.size.reduced)};
+  text-align: start;
+  width: 100%;
+
+  @media (min-width: ${em(SIZES.md)}) {
+    text-align: center;
+
+    img,
+    svg {
+      width: ${({ theme }) => em(theme.honeycomb.size.huge, theme.honeycomb.size.reduced)};
+      height: ${({ theme }) => em(theme.honeycomb.size.huge, theme.honeycomb.size.reduced)};
+    }
+  }
 `;
