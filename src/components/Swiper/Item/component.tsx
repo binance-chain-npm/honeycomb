ef09ca68ec46404ed98193032420550f94f719bc
Jessica Xiao <jessicaoaix@gmail.com>
2021-02-16 00:38:42 +1300

    feat(Swiper): Remove test ID from context

--- src/components/Swiper/Item/component.tsx
+++ src/components/Swiper/Item/component.tsx
@@ -1,18 +1,14 @@
-import React, { useContext } from 'react';
+import React from 'react';
 import { SwiperSlide } from 'swiper/react';
 
 import { useBuildTestId, Testable } from '../../../modules/test-ids';
-import { Context } from '../context';
 
 export type Props = React.ComponentProps<typeof SwiperSlide> & Testable;
 
 export const Component = ({ 'data-testid': testId, ...otherProps }: Props) => {
-  const { testId: parentTestId } = useContext(Context);
-  const { buildTestId } = useBuildTestId({
-    id: testId ? parentTestId : undefined,
-  });
+  const { buildTestId } = useBuildTestId({ id: testId });
 
-  return <SwiperSlide {...otherProps} data-testid={buildTestId(testId)} />;
+  return <SwiperSlide {...otherProps} data-testid={buildTestId()} />;
 };
 
 // The `swiper` code uses `displayName` to render slides correctly, so we cannot change it.
