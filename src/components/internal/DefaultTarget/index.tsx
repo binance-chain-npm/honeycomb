ca809a3a9ebca7eb68b229d7eeb3a7604c09fbfc
Jessica Xiao <jessicaoaix@gmail.com>
2021-02-15 17:05:14 +1300

    refactor: Clean up code

--- src/components/internal/DefaultTarget/index.tsx
+++ src/components/internal/DefaultTarget/index.tsx
@@ -5,17 +5,18 @@ import { styleless } from '../../Styleless';
 import { boxSizing } from '../../../modules/box-sizing';
 
 export const defaultTarget = css`
+  ${styleless};
+  ${boxSizing};
+
   width: 100%;
   background: ${({ theme }) => theme.honeycomb.color.bg.input.normal};
+  font-size: ${({ theme }) => em(theme.honeycomb.size.reduced)};
   border-radius: ${({ theme }) => em(theme.honeycomb.radius.normal, theme.honeycomb.size.reduced)};
   height: ${({ theme }) => em(theme.honeycomb.size.huge, theme.honeycomb.size.reduced)};
-  font-size: ${({ theme }) => em(theme.honeycomb.size.reduced)};
   cursor: pointer;
   ${({ theme }) => transitions(['background', 'color'], theme.honeycomb.duration.normal)};
 `;
 
 export const DefaultTarget = styled.div`
-  ${styleless};
-  ${boxSizing};
   ${defaultTarget};
 `;
