e2022dd0ddf25638d18a14075a99cecaaa062d66
Jessica Xiao <jessicaoaix@gmail.com>
2021-02-18 18:26:23 +1300

    feat(Steps): Remove test IDs from `Connector` as we don't need it

--- src/components/Steps/Connector/component.tsx
+++ src/components/Steps/Connector/component.tsx
@@ -1,21 +1,17 @@
 import React, { useContext } from 'react';
 
-import { useBuildTestId, Testable } from '../../../modules/test-ids';
 import { Context } from '../context';
 
 import { Styled } from './styled';
 
-export type Props = Testable & {
+export type Props = {
   size?: number;
 };
 
-export const Component = ({ size, 'data-testid': testId, ...otherProps }: Props) => {
-  const { buildTestId } = useBuildTestId({ id: testId });
+export const Component = ({ size, ...otherProps }: Props) => {
   const { orientation } = useContext(Context);
 
-  return (
-    <Styled {...otherProps} size={size} $orientation={orientation} data-testid={buildTestId()} />
-  );
+  return <Styled {...otherProps} size={size} $orientation={orientation} />;
 };
 
 Component.displayName = 'Steps.Connector';
