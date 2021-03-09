92ea6513918d94dc976354bc70f3e2bb638bcc27
Jessica Xiao <jessicaoaix@gmail.com>
2021-03-09 22:33:41 +1300

    fix(Modal): Fix memory leak caused by modal close transition (#887)

--- src/components/Modal/component.tsx
+++ src/components/Modal/component.tsx
@@ -96,24 +96,6 @@ export const Component = ({
         closeTimeoutMS={CLOSE_MODAL_TIMEOUT}
         shouldCloseOnEsc={closeOnEsc}
         shouldCloseOnOverlayClick={closeOnBackgroundClick}
-        contentElement={(props, contentElement) => (
-          <>
-            {boxTransitions.map(({ item, props: transitionProps, key }) =>
-              item ? (
-                <Box
-                  {...props}
-                  as={animated.div}
-                  key={key}
-                  style={transitionProps}
-                  position={position}
-                  data-testid={buildTestId()}
-                >
-                  {contentElement}
-                </Box>
-              ) : null,
-            )}
-          </>
-        )}
         overlayElement={(props, contentElement) => (
           <>
             {containerTransitions.map(({ item, props: transitionProps, key }) =>
@@ -126,7 +108,19 @@ export const Component = ({
           </>
         )}
       >
-        <Context.Provider value={context}>{children}</Context.Provider>
+        {boxTransitions.map(({ item, props: transitionProps, key }) =>
+          item ? (
+            <Box
+              as={animated.div}
+              key={key}
+              style={transitionProps}
+              position={position}
+              data-testid={buildTestId()}
+            >
+              <Context.Provider value={context}>{children}</Context.Provider>
+            </Box>
+          ) : null,
+        )}
       </StyledReactModal>
     </>
   );
