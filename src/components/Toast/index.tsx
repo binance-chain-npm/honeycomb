a62a7f246778037ba24dc6f9a39bed1f46b7405a
Jessica Xiao <jessicaoaix@gmail.com>
2021-03-12 01:03:54 +1300

    style: Re-order imports

--- src/components/Toast/index.tsx
+++ src/components/Toast/index.tsx
@@ -1,6 +1,6 @@
-export { createToast } from '../Toast/actions';
 import { Component } from './component';
 import { Icon } from './Icon';
+export { createToast } from '../Toast/actions';
 
 export const Toast = Component as typeof Component & { Icon: typeof Icon };
 
