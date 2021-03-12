371f95475af5204be2847eab91a59a15c6aadddb
Jessica Xiao <jessicaoaix@gmail.com>
2021-03-12 19:05:51 +1300

    fix: Remove logging, turn width values into constant

--- src/components/Toast/actions/styled.tsx
+++ src/components/Toast/actions/styled.tsx
@@ -3,6 +3,11 @@ import { em } from 'polished';
 
 import { SIZES as WINDOW_SIZES } from '../../internal/useWindowSize';
 
+const WIDTHS = {
+  reduced: 280,
+  normal: 380,
+};
+
 export const mdScreen = `min-width: ${em(WINDOW_SIZES.md)}`;
 
 export const SIZES = ['normal', 'reduced'] as const;
@@ -15,12 +20,7 @@ interface Props {
 }
 
 export const toastSize = ({ theme, hasCloseButton, size }: Props) => {
-  const width = (() => {
-    console.log(size);
-    if (size === 'reduced') return 280;
-    return 380;
-  })();
-  return hasCloseButton ? width - theme.honeycomb.size.huge : width;
+  return hasCloseButton ? WIDTHS[size] - theme.honeycomb.size.huge : WIDTHS[size];
 };
 
 export const Styled = styled.div<Props>`
