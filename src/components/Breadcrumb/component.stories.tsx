ae8921dadf3a8fd21d4caf007a8a37e5ef8a5088
Jessica Xiao <jessicaoaix@gmail.com>
2021-03-25 21:24:17 +1300

    feat: Add functional example in stories

--- src/components/Breadcrumb/component.stories.tsx
+++ src/components/Breadcrumb/component.stories.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useMemo, useState } from 'react';
 
 import { decorators } from '../../modules/decorators';
 import { Sections } from '../../modules/sections';
@@ -11,13 +11,46 @@ export default {
   title: `${Sections.Elements}/Breadcrumb`,
 };
 
+const map: { [key: string]: string } = {
+  '/': 'Home',
+  '/page-a': 'Page A',
+  '/page-b': 'Page B',
+  '/page-c': 'Page C',
+};
+
 export const Default = () => {
+  const [page, setPage] = useState('Page C');
+
+  const breadcrumbs = useMemo(() => {
+    const res: React.ReactNode[] = [];
+    for (const [key, value] of Object.entries(map)) {
+      res.push(
+        <Breadcrumb.Item key={key} href={key} onClick={() => setPage(value)}>
+          {value}
+        </Breadcrumb.Item>,
+      );
+      if (value === page) break;
+    }
+    return res;
+  }, [page]);
+
+  const buttons = useMemo(() => {
+    const res: React.ReactNode[] = [];
+    for (const [key, value] of Object.entries(map)) {
+      res.push(
+        <button key={key} onClick={() => setPage(value)}>
+          Go to {value}
+        </button>,
+      );
+    }
+    return res;
+  }, []);
+
   return (
-    <Breadcrumb>
-      <Breadcrumb.Item>Home</Breadcrumb.Item>
-      <Breadcrumb.Item>Page A</Breadcrumb.Item>
-      <Breadcrumb.Item>Page B</Breadcrumb.Item>
-      <Breadcrumb.Item>Page C</Breadcrumb.Item>
-    </Breadcrumb>
+    <>
+      <Breadcrumb>{breadcrumbs}</Breadcrumb>
+      <h1>{page}</h1>
+      {buttons}
+    </>
   );
 };
