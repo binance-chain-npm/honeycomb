504a8d1eaa42f9d45b47cc754b1999e1aa57566f
Jessica Xiao <jessicaoaix@gmail.com>
2021-04-22 00:16:12 +1200

    refactor: Add default margin to docs component container

--- src/components/internal/Docs/DocsComponentContainer/index.tsx
+++ src/components/internal/Docs/DocsComponentContainer/index.tsx
@@ -4,4 +4,5 @@ import { em } from 'polished';
 export const DocsComponentContainer = styled.div`
   padding: ${({ theme }) => em(theme.honeycomb.size.normal)};
   background: ${({ theme }) => theme.honeycomb.color.text.normal};
+  margin: ${({ theme }) => em(theme.honeycomb.size.increased)} 0;
 `;
