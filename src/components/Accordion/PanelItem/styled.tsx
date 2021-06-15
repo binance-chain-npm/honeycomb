587ca418c1ae479bb262aef1b824eb1ad7c3e4c0
Jessica Xiao <jessicaoaix@gmail.com>
2021-06-15 22:25:53 +1200

    fix(Accordion): Syntax errors

--- src/components/Accordion/PanelItem/styled.tsx
+++ src/components/Accordion/PanelItem/styled.tsx
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 export const Styled = styled.div``;
 
 export const Content = styled.div`
-  overflow hidden;
-  will-change height;
-  transition height ${({ theme }) => theme.honeycomb.duration.normal} ease-in-out;
+  overflow: hidden;
+  will-change: height;
+  transition: height ${({ theme }) => theme.honeycomb.duration.normal} ease-in-out;
 `;
