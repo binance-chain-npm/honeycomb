7f4a25202155f4e1f4d238b3dfe7ed48fd50dfae
Jessica Xiao <jessicaoaix@gmail.com>
2021-10-14 20:29:49 +1300

    fix(Switch): Make component controlled to fix inconsistent render issue (#285)

--- src/components/Switch/component.tsx
+++ src/components/Switch/component.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useMemo, useState } from 'react';
+import React, { useMemo } from 'react';
 import { nanoid } from 'nanoid';
 
 import { useBuildTestId, Testable } from '../../modules/test-ids';
@@ -6,14 +6,16 @@ import { Input, LabelContent, Styled } from '../Checkbox/styled';
 
 import { Control, Label, Size } from './styled';
 
-export type Props = Omit<React.InputHTMLAttributes<HTMLInputElement>, 'size' | 'type'> &
+export type Props = Omit<React.InputHTMLAttributes<HTMLInputElement>, 'checked' | 'size' | 'type'> &
   Testable & {
+    checked: boolean;
     label?: string;
     size?: Size;
   };
 
 export const Component = ({
   className,
+  checked,
   label,
   size = 'huge',
   'data-testid': testId,
@@ -22,26 +24,10 @@ export const Component = ({
   const { buildTestId } = useBuildTestId({ id: testId });
   const id = useMemo(() => otherProps.id || `id-${nanoid()}`, [otherProps.id]);
 
-  const [checked, setChecked] = useState(false);
-
-  const check = useCallback<NonNullable<Props['onChange']>>(
-    (evt) => {
-      setChecked((value) => !value);
-      otherProps.onChange?.(evt);
-    },
-    [otherProps.onChange],
-  );
-
   return (
     <Styled className={className}>
-      <Input
-        {...otherProps}
-        id={id}
-        type="checkbox"
-        onChange={check}
-        data-testid={buildTestId('native-input')}
-      />
-      <Label htmlFor={id} size={size} data-testid={buildTestId('label')}>
+      <Input {...otherProps} id={id} type="checkbox" data-testid={buildTestId('native-input')} />
+      <Label htmlFor={id} size={size} checked={checked} data-testid={buildTestId('label')}>
         <Control size={size} checked={checked} />
         {!!label && <LabelContent data-testid={buildTestId('label-content')}>{label}</LabelContent>}
       </Label>
