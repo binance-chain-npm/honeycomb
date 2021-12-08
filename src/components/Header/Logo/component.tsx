7d8946c4628235e1b316ab923784128c04078db5
Jessica Xiao <jessicaoaix@gmail.com>
2021-12-09 00:54:03 +1300

    fix(Dropdown.DefaultTarget): Fix dropdown caret alignment (#1444) (#297)

--- src/components/Header/Logo/component.tsx
+++ src/components/Header/Logo/component.tsx
@@ -7,15 +7,16 @@ import { Space } from '../../Space';
 import { Styled, Logo, LogoSmall, StyledBadge } from './styled';
 
 export type Props = Testable & {
+  className?: string;
   text?: React.ReactNode;
 };
 
-export const Component = ({ text, 'data-testid': testId }: Props) => {
+export const Component = ({ text, 'data-testid': testId, ...otherProps }: Props) => {
   const { buildTestId } = useBuildTestId({ id: testId });
   const { isSm } = useWindowSize();
 
   return (
-    <Styled data-testid={buildTestId()}>
+    <Styled data-testid={buildTestId()} {...otherProps}>
       {isSm ? (
         <Logo data-testid={buildTestId('logo-sm')} />
       ) : (
